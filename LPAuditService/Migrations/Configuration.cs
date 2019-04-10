2d93833431d865c41b2e1c1e32c0fe8535c75673
Manthra <daniel.1912@hotmail.com>
2019-04-10 10:11:18 -0500

    Añadida la propiedad dte_LastDateCreated para controlar las fechas en que se crearon los checklists

--- LPAuditService/Migrations/Configuration.cs
+++ LPAuditService/Migrations/Configuration.cs
@@ -1,9 +1,6 @@
 namespace LPAuditService.Migrations
 {
-    using System;
-    using System.Data.Entity;
     using System.Data.Entity.Migrations;
-    using System.Linq;
 
     internal sealed class Configuration : DbMigrationsConfiguration<LPAuditService.Models.LayoutProcessContext>
     {
