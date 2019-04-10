0a8feb781ac963094b406637fa18192877977fc0
Manthra <daniel.1912@hotmail.com>
2019-04-10 10:06:18 -0500

    Modificado entity framework para que se creen migrations automaticas

--- LPAuditService/Migrations/Configuration.cs
+++ LPAuditService/Migrations/Configuration.cs
@@ -9,7 +9,7 @@ namespace LPAuditService.Migrations
     {
         public Configuration()
         {
-            AutomaticMigrationsEnabled = false;
+            AutomaticMigrationsEnabled = true;
         }
 
         protected override void Seed(LPAuditService.Models.LayoutProcessContext context)
